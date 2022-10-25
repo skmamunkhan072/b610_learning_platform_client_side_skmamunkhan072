b8501467a3c9f474d2193373869f64b4b13d2da5
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-10-26 02:11:17 +0600

    coures ditail card add and responsip

--- src/components/Pages/Trainers/Trainers.jsx
+++ src/components/Pages/Trainers/Trainers.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import CoursesDetails from "../../share/CoursesDetails/CoursesDetails";
 
 const Trainers = () => {
   return (
     <div className="flex">
-      <div className="flex flex-col h-full p-3 w-[30%] dark:bg-gray-900 dark:text-gray-100 rounded-r-lg"></div>
+      <div className="flex flex-col h-full p-3 w-[30%] dark:bg-gray-900 dark:text-gray-100 rounded-r-lg pl-5 text-start"></div>
       <div className="text-center">
         <p className="text-center">ths is details</p>
       </div>
--- src/components/share/CoursesDetails/CoursesDetails.jsx
+++ src/components/share/CoursesDetails/CoursesDetails.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import CoursesDetailsCard from "../CoursesDetailsCard/CoursesDetailsCard";
 import "./CoursesDetails.css";
 const CoursesDetails = () => {
   return (
-    <div>
-      <h1>Courses Details </h1>
+    <div className="md:flex">
+      <div className="md:flex md:flex-col h-full p-3  md:w-[40%] lg:w-[50%] xl:w-[40%]  dark:bg-gray-900 dark:text-gray-100 rounded-r-lg pl-5 text-start">
+        <CoursesDetailsCard />
+        <CoursesDetailsCard />
+        <CoursesDetailsCard />
+        <CoursesDetailsCard />
+        <CoursesDetailsCard />
+      </div>
+      <div className="text-center">
+        <p className="text-center">ths is details</p>
+      </div>
     </div>
   );
 };
