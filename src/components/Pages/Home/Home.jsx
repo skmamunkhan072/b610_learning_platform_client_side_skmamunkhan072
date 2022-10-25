dadbbbaaf4c003d1beec531fe3ecdcf325edef8b
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-10-25 21:56:43 +0600

    suing up page add .

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { TbArrowRightTail } from "react-icons/tb";
+import HomeImg from "../../../img/shea_img/Homeimg.png";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is Home compo</h1>
+      <div className="relative">
+        <img
+          src="https://images.pexels.com/photos/3228766/pexels-photo-3228766.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          className="absolute inset-0 object-cover w-full h-full"
+          alt=""
+        />
+        <div className="relative bg-opacity-75 bg-deep-purple-accent-700 bg-[#661fff]">
+          <svg
+            className="absolute inset-x-0 bottom-[-1px] text-[#2A303C]"
+            viewBox="0 0 1160 163"
+          >
+            <path
+              fill="currentColor"
+              d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
+            />
+          </svg>
+          <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="flex flex-col items-center justify-between xl:flex-row">
+              <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
+                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                  Learning Today, <br className="hidden md:block" />
+                  Leading Tomorrow
+                </h2>
+                <p className="max-w-xl mb-4 text-base text-gray-200 md:text-lg">
+                  Here web design, graphics design, and full stack web
+                  development are taught online.
+                </p>
+                <Link
+                  to="/singUp"
+                  aria-label=""
+                  className="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-teal-accent-400 hover:text-teal-accent-700"
+                >
+                  Ready to get get started?
+                  <TbArrowRightTail className="inline-block text-2xl mt-[6px] ml-1" />
+                </Link>
+              </div>
+              <div className="w-full max-w-xl xl:w-5/12">
+                {/* <div className="rounded shadow-2xl "> */}
+                <img src={HomeImg} alt="" className="rounded shadow-2xl" />
+                {/* </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/share/Header/Header.jsx
+++ src/components/share/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { AiOutlineSketch } from "react-icons/ai";
 import "./Header.css";
 
 const Header = () => {
@@ -9,25 +10,11 @@ const Header = () => {
     console.log(event.target);
   };
   return (
-    <div className="bg-gray-900 mb-5">
+    <div className="bg-gray-900">
       <div className="px-4 py-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="relative flex items-center justify-between">
           <Link to="/" className="inline-flex items-center">
-            <svg
-              className="w-8 text-teal-accent-400"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              stroke="currentColor"
-              fill="none"
-            >
-              <rect x="3" y="1" width="7" height="12" />
-              <rect x="3" y="17" width="7" height="6" />
-              <rect x="14" y="1" width="7" height="6" />
-              <rect x="14" y="11" width="7" height="12" />
-            </svg>
+            <AiOutlineSketch className="text-5xl text-teal-accent-400" />
             <div className="ml-2 text-start text-xl font-bold tracking-wide text-gray-100 uppercase">
               <p>Study Course</p>
               <small>Journey to success</small>
@@ -60,14 +47,6 @@ const Header = () => {
             </li>
             <li>
               <Link
-                to="/trainers"
-                className="font-medium tracking-wide text-gray-100 cursor-pointer transition-colors duration-200 hover:text-deep-purple-accent-400"
-              >
-                Trainers
-              </Link>
-            </li>
-            <li>
-              <Link
                 to="/pricing"
                 className="font-medium tracking-wide text-gray-100 cursor-pointer transition-colors duration-200 hover:text-deep-purple-accent-400"
               >
@@ -84,8 +63,8 @@ const Header = () => {
             </li>
             <li>
               <Link
-                to="/singup"
-                className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                to="/SingUp"
+                className="font-medium tracking-wide text-gray-100 cursor-pointer transition-colors duration-200 hover:text-deep-purple-accent-400"
               >
                 Sign up
               </Link>
@@ -183,14 +162,6 @@ const Header = () => {
                       </li>
                       <li>
                         <Link
-                          to="/trainers"
-                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                        >
-                          Trainers
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
                           to="/pricing"
                           className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
@@ -207,9 +178,8 @@ const Header = () => {
                       </li>
                       <li>
                         <Link
-                          to="/singup"
-                          className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide
-                          bg-gray-700 text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                          to="/SingUp"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
                           Sign up
                         </Link>
